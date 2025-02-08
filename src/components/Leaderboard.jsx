2e8f29dc39bf3f226b3708e21e0e65ad9d09fae2
Brennan Davenport <brennandavenport9@gmail.com>
2025-02-07 22:03:47 -0500

    fixed route

---
+++ src/components/Leaderboard.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+const Leaderboard = () => {
+    return(
+        <h1>Leaderboard Page</h1>
+    )
+}
+
+export default Leaderboard;
---
+++ src/components/Swipe-Learn.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+const SwipeLearn = () => {
+    return(
+        <h1>Swipe Learn Page</h1>
+    )
+}
+
+export default SwipeLearn;
