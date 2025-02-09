c0f66611108b67950d9a736dd3d56bdb0e1725fb
Brennan Davenport <brennandavenport9@gmail.com>
2025-02-08 20:01:07 -0500

    added richard leaderboard

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -7,10 +7,33 @@ const Leaderboard = () => {
         <div>
           <NavBar></NavBar>
           <div className="page-content">
-            <h1>Leaderboard Content</h1>
+            <h1>Leaderboard</h1>
+            <h2>Top 10 All Time</h2>
+            <div class="leaderboard">
+                <div class="player">
+                    <div class="info">
+                        <span class="rank">1.</span>
+                        <span class="name">Rich Baby Daddy </span>
+                    </div>
+                    <div class="roi positive">+25.3%</div>
+                </div>
+                <div class="player">
+                    <div class="info">
+                        <span class="rank">2.</span>
+                        <span class="name">Arjun</span>
+                    </div>
+                    <div class="roi positive">+18.7%</div>
+                </div>
+                <div class="player">
+                    <div class="info">
+                        <span class="rank">3.</span>
+                        <span class="name">Brennan</span>
+                    </div>
+                    <div class="roi negative">-1023432.2%</div>
+                </div>
+            </div>
           </div>
-        </div>
-        
+        </div>  
     )
 }
 
